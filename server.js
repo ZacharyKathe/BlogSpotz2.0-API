6c4bdadc49c4fef30fce518aa7a5ff0d5e60352f
ZacharyKathe <zacharykathe@gmail.com>
2021-06-30 15:22:00 -0700

    saving server/connection set up

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const logger = require('morgan');
 const cors = require('cors');
-//put .env require here----
 
-/// CREATE .CONFIG .CONNECTION FOLDER/FILE AND REQUIRE
+const sequelize = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
